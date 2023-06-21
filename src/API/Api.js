6dc50cff81fd9087126e574aaeeb21e8aa7335e2
19alexandr80 <usanka1980@gmail.com>
2023-06-21 16:17:42 +0300

    api

---
+++ src/API/Api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+const keyApi = '6d070ee2b82df1d970d23eca62ef69da';
+
+// ===================================================================================
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.themoviedb.org/3/trending/all/day?language=en-US',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDA3MGVlMmI4MmRmMWQ5NzBkMjNlY2E2MmVmNjlkYSIsInN1YiI6IjY0OTExN2NmNDJiZjAxMDExZTc1MTQzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S6VKiZqFJ17KU30nJMe8ewgWIwV0rkd-iqWQtxFXFF0',
+//   },
+// };
+//======================================================================================
+export class NewApi {
+  async getUser(page = 1) {
+    try {
+      const response = await axios.get(
+        `/trending/all/day?language=en-US/&api_key=${keyApi}&page=${page}`
+      );
+      return response.data;
+
+      // ==============================================================================
+      // const response = await axios.request(options);
+      // return response;
+      // ============================================================================
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getAmountOfElements() {
+    return this.amountOfElements;
+  }
+  getTotalHits() {
+    return this.totalHits;
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,83 @@
-import { Routes, NavLink, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, NavLink, Route, Link, Outlet } from 'react-router-dom';
+
+import { NewApi } from 'API/Api';
+const api = new NewApi();
 
 export const App = () => {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState(false);
+  useEffect(() => {
+    const getApi = async () => {
+      setStatus(true);
+      try {
+        const data = await api.getUser(page);
+
+        setList(prev => {
+          return [...prev, ...data.results];
+        });
+      } catch (error) {
+        console.error(error.messeng);
+      } finally {
+        setStatus(false);
+        return;
+      }
+    };
+    getApi();
+  }, []);
+  console.log(list);
+  // const fun = async () => {
+  //   try {
+  //     // const data = await api.getUser();
+  //     // setList(prev => {
+  //     //   return [...prev, ...data.results];
+  //     // });
+  //     console.log(list);
+  //   } catch (error) {
+  //     console.error(error.messeng);
+  //   }
+  // };
+  // fun();
   return (
     <div>
       <nav>
-        <NavLink to="/">Home page</NavLink>
-        <NavLink to="/collect">Collection</NavLink>
+        <ul>
+          <li>
+            <NavLink to="/">Home page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/:movieId">MovieDetails</NavLink>
+          </li>
+        </ul>
       </nav>
       <Routes>
         <Route path="/" element={<div>HOME PAGE</div>} />
-        <Route path="/collect" element={<div>COLLECTION</div>} />
+        <Route path="/movies" element={<div>MOVIES</div>} />
+        <Route
+          path="/movies/:movieId"
+          element={
+            <div>
+              <h2>MovieDetails</h2>
+              <ul>
+                <li>
+                  <Link to="cast">Cast</Link>
+                </li>
+                <li>
+                  <Link to="reviews">Reviews</Link>
+                </li>
+              </ul>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path="cast" element={<div>CAST</div>} />
+          <Route path="reviews" element={<div>REVIEWS</div>} />
+        </Route>
       </Routes>
-      React homework template
     </div>
   );
 };
